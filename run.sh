3eb5e484fc5c701462fe1820d8f24f9666f0d47e
Your Name <you@example.com>
2018-12-01 22:49:04 -0500

    add scripta

---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+bash ./run_compare_negative.sh &
+bash ./run_cardiac_vs_eps.sh &
+bash ./run_prostate_vs_eps.sh
---
+++ run_cardiac_vs_eps.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=0 python train.py --device=cuda --dataroot=cardiac --optim_inner_loop_num=3 --stop_dilation_epoch=200 --ignore_negative=False --eps=0.01 &
+
+OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=1 python train.py --device=cuda --dataroot=cardiac --optim_inner_loop_num=3 --stop_dilation_epoch=200 --ignore_negative=False --eps=0.1 &
+
+OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=2 python train.py --device=cuda --dataroot=cardiac --optim_inner_loop_num=3 --stop_dilation_epoch=200 --ignore_negative=False --eps=0.2 &
+
+OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=3 python train.py --device=cuda --dataroot=cardiac --optim_inner_loop_num=3 --stop_dilation_epoch=200 --ignore_negative=False --eps=0.4 &
+
+OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=1 python train.py --device=cuda --dataroot=cardiac --optim_inner_loop_num=3 --stop_dilation_epoch=200 --ignore_negative=False --eps=0.6 &
+
+OMP_NUM_THREADS=4 CUDA_VISIBLE_DEVICES=3 python train.py --device=cuda --dataroot=cardiac --method fullysupervised
