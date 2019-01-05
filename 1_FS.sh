4b6f6a4523a20968842261c3cdab0d672c0dccd3
jizong <pengjizong@sjtu.edu.cn>
2019-01-05 16:01:59 -0500

    fix some petit bugs

--- 1_FS.sh
+++ 1_FS.sh
@@ -8,10 +8,10 @@ python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=
 python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_0.0 --eps=0.0  &
 
 python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_Daug_0.1  --eps=0.1  &
-python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_0.4 --eps=0.1 &
+python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_0.1 --eps=0.1 &
 
 python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_Daug_0.2 --eps=0.2 &
-python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_0.4 --eps=0.2 &
+python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_0.2 --eps=0.2 &
 
 python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_Daug_0.4 --eps=0.4 &
 python train.py --device=cuda  --dataroot=cardiac --method=admm_size --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/size_enet_0.4 --eps=0.4 
@@ -51,7 +51,7 @@ python train_in.py --device=cuda  --dataroot=cardiac --method=admm_size_in --dat
 python train_in.py --device=cuda  --dataroot=cardiac --method=admm_gc_size_in --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/gcsizeIN_enet_Daug_0.0 --eps=0.0 &
 python train_in.py --device=cuda  --dataroot=cardiac --method=admm_gc_size_in --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/gcsizeIN_enet_0.0 --eps=0.0 &
 
-python train_in.py --device=cuda  --dataroot=cardiac --method=admm_gc_size_in --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/gcsizeIN_enet_Daug_0.1--eps=0.1 &
+python train_in.py --device=cuda  --dataroot=cardiac --method=admm_gc_size_in --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/gcsizeIN_enet_Daug_0.1 --eps=0.1 &
 python train_in.py --device=cuda  --dataroot=cardiac --method=admm_gc_size_in --data_aug=False --arch=enet --max_epoch=80 --save_dir=results/cardiac/gcsizeIN_enet_0.1 --eps=0.1 &
 
 python train_in.py --device=cuda  --dataroot=cardiac --method=admm_gc_size_in --data_aug=True --arch=enet --max_epoch=80 --save_dir=results/cardiac/gcsizeIN_enet_Daug_0.2 --eps=0.2 &
