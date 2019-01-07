4adca98985949fc737f46242ff3b14e0263acec5
Jizong Ubuntu <jizong.peng.1@etsmtl.net>
2019-01-07 15:47:01 -0500

    add script control

--- run.sh
+++ run.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-fullysupervised
-weakly_supervised (2dataset)
-weakly_supervisedwith_inequaility (2dataset)(diffferent eps)
-
-show images
-
+CUDA_VISIBLE_DEVICES=1 bash train_script.sh train cardiac True 80 False True &
+CUDA_VISIBLE_DEVICES=2 bash train_script.sh train_in cardiac True 80 True False &
+CUDA_VISIBLE_DEVICES=2 bash train_script.sh train_in cardiac True 80 False True
---
+++ train_script.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+train_main=$1
+if [[ $train_main != "train" && $train_main != "train_in" ]];
+then
+    echo "Avaliable scripts are train and train_in, given {$train_main}"
+    exit 1
+fi
+
+dataset=$2
+if [[ $dataset != "cardiac" && $dataset != "prostate" ]];
+then
+    echo "Avaliable datasets cardiac and prostate, given {$dataset}"
+    exit 1
+fi
+dataaug=$3
+if [[ $dataaug != "True" && $dataaug != "False" ]];
+then
+    echo "Avaliable dataAug options are True or False, given {$dataaug}"
+    exit 1
+fi
+max_epoch=$4
+if [[ $max_epoch -gt 200 ]];
+then
+    echo "max_epoch should be less than 200 , given {$max_epoch}"
+    exit 1
+fi
+size_run=$5
+if [[ $size_run != "True" && $size_run != "False" ]];
+then
+    echo "Avaliable size options are True or False, given {$size_run}"
+    exit 1
+fi
+gc_size_run=$6
+if [[ $gc_size_run != "True" && $gc_size_run != "False" ]];
+then
+    echo "Avaliable gc_size options are True or False, given {$gc_size_run}"
+    exit 1
+fi
+
+
+echo "train_main:" $train_main
+echo "dataset:" $dataset
+echo "dataaug:" $dataaug
+echo "max_epoch:"$max_epoch
+
+
+if [[ $train_main == "train" ]];
+then
+echo
+#    python $train_main".py" --device=cuda  --dataroot=$dataset --method=fullysupervised --data_aug=$dataaug --arch=enet --loss=cross_entropy --max_epoch=$max_epoch --save_dir=results/cardiac/FS_enet_Daug_$dataaug --num_admm_innerloop=1
+fi
+
+if [[ $train_main == "train_in" ]];
+then
+    method_postfix="_in"
+
+fi
+echo "method_postfix" $method_postfix
+
+
+
+
+### with only size constraints:
+if [[ $size_run == "True"  ]];
+then
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_size$method_postfix --data_aug=$dataaug --arch=enet  --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_size_enet_Daug_"$dataaug"_0.0" --eps=0.0 &
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_size_enet_Daug_"$dataaug"_0.1"  --eps=0.1  &
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_size_enet_Daug_"$dataaug"_0.2" --eps=0.2 &
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_size_enet_Daug_"$dataaug"_0.4" --eps=0.4
+else
+echo "size is not estimated."
+
+fi
+
+
+## with both
+if [[ $gc_size_run == "True" ]];
+then
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_gc_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_gcsize_enet_Daug_"$dataaug"_0.0" --eps=0.0 &
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_gc_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_gcsize_enet_Daug_"$dataaug"_0.1"  --eps=0.1 &
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_gc_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_gcsize_enet_Daug_"$dataaug"_0.2" --eps=0.2 &
+
+python $train_main".py" --device=cuda  --dataroot=$dataset --method=admm_gc_size$method_postfix --data_aug=$dataaug --arch=enet --max_epoch=$max_epoch --save_dir=results/cardiac/$train_main"_gcsize_enet_Daug_"$dataaug"_0.4" --eps=0.4
+
+else
+echo "gc_size is not estimated."
+fi
